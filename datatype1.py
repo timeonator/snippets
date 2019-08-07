dd6576249ffad7fadafb9ca563026f7e656731a6
Robert Gifford <timeonator@gmail.com>
2019-08-07 13:04:02 -0700

    Simple Datatype and IO demonstration in Python

---
+++ datatype1.py
@@ -0,0 +1,25 @@
+# python3 simple datatype example
+# 
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+
+# Read and save an integer, double, and String to your variables.
+import sys
+
+# x = int(sys.stdin.readline())
+# y = float(sys.stdin.readline())
+# greating = sys.stdin.readline()
+
+print(i+int(sys.stdin.readline()))
+print(d+float(sys.stdin.readline()))
+print(s+sys.stdin.readline())
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
